441075e8650b88a83f9aa3bc82b2956e5a6e2db2
elenadj7 <eelenadjurdjevic@gmail.com>
2023-11-07 14:02:43 +0100

    initial commit

---
+++ Forms/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace hci_restaurant.Forms
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            App app = new App();
+            StartWindow mainWindow = new StartWindow();
+            app.Run(mainWindow);
+        }
+    }
+}
