969ad6bec64fa38fd7dfb31c38c0ecd0e88d5ef8
elenadj7 <eelenadjurdjevic@gmail.com>
2023-11-07 14:57:58 +0100

    db config

---
+++ Data/MySQL/MySQLUtil.cs
@@ -0,0 +1,46 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace hci_restaurant.Data.MySQL
+{
+    internal class MySQLUtil
+    {
+        private static readonly string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+        public static MySqlConnection? GetMySQLConnection()
+        {
+            MySqlConnection? connection = null;
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return connection;
+        }
+
+        public static void Close(MySqlConnection connection)
+        {
+            connection?.Close();
+        }
+
+        public static void Close(MySqlDataReader reader)
+        {
+            reader?.Close();
+        }
+
+        public static void Close(MySqlDataReader reader, MySqlConnection connection)
+        {
+            Close(reader);
+            Close(connection);
+        }
+    }
+}
--- Forms/StartWindow.xaml.cs
+++ Forms/StartWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hci_restaurant.Data.MySQL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
